2a0e1134f3b93a51a9d1c8a4303aeef5816b1d60
cchun01 <63212924+cchun01@users.noreply.github.com>
2022-02-10 12:04:04 -0800

    link style change

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -3,12 +3,20 @@ import SignUp from './SignUp'
 import LogIn from './LogIn';
 import Home from './Home';
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import styled from "styled-components";
+
+const linkStyle = {
+  margin: "1rem",
+  textDecoration: "none",
+  color: 'green'
+  
+};
 
 function MyApp() {
     return (
       <Router>
         <nav>
-          <Link to="/"> Home </Link>
+          <Link to="/" style={linkStyle}> Home </Link>
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
