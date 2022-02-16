dc3a26eedd9a10b4ff189760b0d12602771df7df
Rachel Izenson <rjizenson@gmail.com>
2022-02-16 13:11:24 -0800

    more prettier

--- react-frontend/src/AboutUs.js
+++ react-frontend/src/AboutUs.js
@@ -1,51 +1,52 @@
-import './AboutUs.css';
+import "./AboutUs.css";
 
 function AboutUs() {
-    return (
-      <div>
-        <div className="headerAbout">About Us</div>
-        <div className="pinkblock">
-            Potion is designed and created by four Cal Poly students in CSC 307.
-            Through learning and applying our skills around software engineering,
-            we created a productivity platform focusing on minimalism aimed to help
-            users feel organized.
-        </div>
-        <div className="yellowblock">
-            Our project is deeply inspired by the Notion app, but we add a unique magical spin.
-            Potion brings simplicity, style, and substance to people. Our service is truly customizable
-            for all users and thrives based on it's versatility. We hope you enjoy Potion as much as we
-            did creating it.
-        </div>
-        <div className="imagecontainer">
-            <div className= "row">
-                <div class="column">
-                    <figure>
-                        <img src={require('./rachel.png')}/>
-                        <figcaption>Rachel</figcaption>
-                    </figure>   
-                </div>
-                <div class="column">
-                    <figure>
-                        <img src={require('./dani.JPEG')} />
-                        <figcaption>Dani</figcaption>
-                    </figure>
-                </div>
-                <div class="column">
-                    <figure>
-                        <img src={require('./alexa.JPG')} />
-                        <figcaption>Alexa</figcaption>
-                    </figure>
-                </div>
-                <div class="column">
-                    <figure>
-                        <img src={require('./colin.jpeg')} />
-                        <figcaption>Colin</figcaption>
-                    </figure>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="headerAbout">About Us</div>
+      <div className="pinkblock">
+        Potion is designed and created by four Cal Poly students in CSC 307.
+        Through learning and applying our skills around software engineering, we
+        created a productivity platform focusing on minimalism aimed to help
+        users feel organized.
+      </div>
+      <div className="yellowblock">
+        Our project is deeply inspired by the Notion app, but we add a unique
+        magical spin. Potion brings simplicity, style, and substance to people.
+        Our service is truly customizable for all users and thrives based on
+        it's versatility. We hope you enjoy Potion as much as we did creating
+        it.
+      </div>
+      <div className="imagecontainer">
+        <div className="row">
+          <div class="column">
+            <figure>
+              <img src={require("./rachel.png")} />
+              <figcaption>Rachel</figcaption>
+            </figure>
+          </div>
+          <div class="column">
+            <figure>
+              <img src={require("./dani.JPEG")} />
+              <figcaption>Dani</figcaption>
+            </figure>
+          </div>
+          <div class="column">
+            <figure>
+              <img src={require("./alexa.JPG")} />
+              <figcaption>Alexa</figcaption>
+            </figure>
+          </div>
+          <div class="column">
+            <figure>
+              <img src={require("./colin.jpeg")} />
+              <figcaption>Colin</figcaption>
+            </figure>
+          </div>
         </div>
       </div>
-    );
-  }
-  
-  export default AboutUs;
+    </div>
+  );
+}
+
+export default AboutUs;
--- react-frontend/src/EmailVerification.js
+++ react-frontend/src/EmailVerification.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./EmailVerification.css";
 
-
 function clicked() {
   const code = document.getElementsByClassName("textboxC")[0].value;
-  if(code === ""){
+  if (code === "") {
     console.log("No code provided");
-  }else{
+  } else {
     console.log("Profile\n\nCode: " + code);
   }
 }
@@ -16,7 +15,10 @@ function EmailVerification() {
     <div>
       <div className="headerLog">Email Verification</div>
       <div className="fieldNameCLog">Enter Code:</div>
-      <div className="fieldNameILog">We sent a code to the provided email. Please enter the code below to authenticate your account.</div>
+      <div className="fieldNameILog">
+        We sent a code to the provided email. Please enter the code below to
+        authenticate your account.
+      </div>
       <input type="text" className="textboxCLog" />
       <button onClick={clicked} className="submitLog">
         Submit
