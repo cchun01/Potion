448a67cca68ab0ecf0a5d8d64543b34171a3d845
Alexa Novaes Nichols <alexanovaesnichols@Alexas-MacBook-Air.local>
2022-02-21 22:05:11 -0800

    Frontend of My Notes page

---
+++ react-frontend/src/MyNotes.js
@@ -0,0 +1,20 @@
+import "./MyNotes.css";
+
+function MyNotes() {
+    return (
+    <div>
+        <div className="myHeader"> My Notes </div>
+        <div className="subHeader"> All your notes are organized here </div>
+        <div className="notesRow">
+            <div className="noteContainer">
+                <div className="noteBoxStyle">
+                    <div className="noteTitle"> + New Note </div>
+                    <div className="noteEmoji"> 📝 </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default MyNotes;
