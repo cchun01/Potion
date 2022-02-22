9a71c8fe139857ed844dd8fc4169c95adc72feca
Alexa Novaes Nichols <alexanovaesnichols@Alexas-MacBook-Air.local>
2022-02-21 22:16:47 -0800

    Frontend of My Notes page

--- react-frontend/src/MyNotes.js
+++ react-frontend/src/MyNotes.js
@@ -4,17 +4,17 @@ function MyNotes() {
   return (
     <div>
       <div className="myHeader">My Notes</div>
-        <div className="subHeader">All your notes are organized here</div>
-          <div className="notesRow">
-            <div className="noteContainer">
-              <div className="noteBoxStyle">
-                <div className="noteTitle">+ New Note</div>
-                  <div className="noteEmoji"> 📝 </div>
-              </div>
-            </div>
+      <div className="subHeader">All your notes are organized here</div>
+      <div className="notesRow">
+        <div className="noteContainer">
+          <div className="noteBoxStyle">
+            <div className="noteTitle">+ New Note</div>
+            <div className="noteEmoji"> 📝 </div>
           </div>
+        </div>
+      </div>
     </div>
-    );
+  );
 }
 
 export default MyNotes;
