12e112f16a3146077a2b3a0f5a28edd08d2f6e99
Alexa Novaes Nichols <70447594+alexa-nov@users.noreply.github.com>
2022-02-24 17:46:52 -0800

    Basic implementation of new note frontend

---
+++ react-frontend/src/NewNote.js
@@ -0,0 +1,82 @@
+import "./NewNote.css";
+import React from 'react';
+import { Textarea } from "@chakra-ui/textarea"
+import { Text } from '@chakra-ui/react'
+
+function NoteTitle() {
+  let [value, setValue] = React.useState('')
+
+  let handleInputChange = (e) => {
+    let inputValue = e.target.value
+    setValue(inputValue)
+  }
+  return (
+    <>
+      <Text mb='8px'>{value}</Text>
+      <Textarea 
+        value={value}
+        onChange={handleInputChange}
+        placeholder='Untitled Note'
+        size='sm'
+      />
+    </>
+  )
+}
+
+function NoteSubheader() {
+  let [value, setValue] = React.useState('')
+
+  let handleInputChange = (e) => {
+    let inputValue = e.target.value
+    setValue(inputValue)
+  }
+  return (
+    <>
+      <Text mb='8px'>{value}</Text>
+      <Textarea
+        value={value}
+        onChange={handleInputChange}
+        placeholder='Editing a new note, the world is your oyster.'
+        size='sm'
+      />
+    </>
+  )
+}
+
+function NoteBox() {
+  let [value, setValue] = React.useState('')
+
+  let handleInputChange = (e) => {
+    let inputValue = e.target.value
+    setValue(inputValue)
+  }
+  return (
+    <>
+      <Text mb='8px'>{value}</Text>
+      <Textarea
+        value={value}
+        onChange={handleInputChange}
+        placeholder='Make a new note, start typing away...'
+        size='sm'
+      />
+    </>
+  )
+}
+
+function NewNote() {
+  return (
+    <div>
+      <div className="myHeader">
+        <NoteTitle/>
+      </div>
+      <div className="subHeader">
+        <NoteSubheader/>
+      </div>
+      <div className="textedit">
+        <NoteBox/>
+      </div>
+    </div>
+  );
+}
+
+export default NewNote;
