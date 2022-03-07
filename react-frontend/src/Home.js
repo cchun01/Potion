21aa259bef980aac19a984b6380ca370624f020c
Dani Lopez <danilopez@Danis-MacBook-Pro.local>
2022-03-07 13:43:24 -0800

    removed unnecessary code and ran prettier

--- react-frontend/src/Home.js
+++ react-frontend/src/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Home.css";
 import "./TryNotionButton";
-import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 const linkStyle = {
